fb32060c3a1ea2f676504f939d1d8ec25e6869cd
Olena Vdovychenko <ena.noname@gmail.com>
2021-07-07 17:50:11 +0300

    Solution

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,38 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof checkPassword('test')).toBe('boolean');
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
-
+    expect(checkPassword('Fairly2.')).toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return 'true' for the valid password with 16 characters`, () => {
+    expect(checkPassword('Ridiculousness2.')).toBeTruthy();
+  });
+
+  it(`should return 'false' for the password without digit`, () => {
+    expect(checkPassword('Cinderella@')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the password without special character`, () => {
+    expect(checkPassword('Cinderella4')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the password without uppercase letter`, () => {
+    expect(checkPassword('cinderella@4')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the password with 5 characters`, () => {
+    expect(checkPassword('Air8@')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the password with 19 characters`, () => {
+    expect(checkPassword('Conceptualisation6@')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the empty value in field "password"`, () => {
+    expect(checkPassword('')).toBeFalsy();
+  });
 });
