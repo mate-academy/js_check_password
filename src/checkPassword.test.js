1b5a8117bb0dc5a1cf3627926f34a2c89297ced3
Iryna Khyzhynska <iryna.khyzhynska@gmail.com>
2021-07-07 18:11:05 +0300

    Solution

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,29 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof checkPassword('111dsf')).toBe('boolean');
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
-
+    expect(checkPassword('P@sswo0d')).toBe(true);
   });
 
-  // write more tests here
+  it(`should return 'false' for the invalid password with 8 characters`, () => {
+    expect(checkPassword('P@sswжжd')).toBe(false);
+  });
+
+  it(`should return 'false' for password less 
+  than 8 characters: 'qwerty'`, () => {
+    expect(checkPassword('q@1erty')).toBe(false);
+  });
+
+  it(`should return 'false' for the invalid password with 
+  more than 16 characters: 'VerySrt@ngpassw1'`, () => {
+    expect(checkPassword('VerySrt@ngpassw1')).toBe(true);
+  });
+
+  it(`should return 'false' for the invalid password with 
+  3 characters: '%7g'`, () => {
+    expect(checkPassword('%7g')).toBe(false);
+  });
 });
