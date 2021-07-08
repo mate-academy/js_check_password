831b547ba3ba88150205c3a4d096d63a484a1a54
coyotious <coyotious@gmail.com>
2021-07-08 19:22:02 +0300

    added tests for 12 symblos and non English

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,45 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof checkPassword('passwordd')).toBe('boolean');
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
-
+    expect(checkPassword('Pa$sw0rd')).toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return 'true' for the valid password with 12 characters`, () => {
+    expect(checkPassword('Pa$sw0rd')).toBeTruthy();
+  });
+
+  it(`should return 'true' for the valid password with 12 characters`, () => {
+    expect(checkPassword('Pa$sw0rд1')).toBeTruthy();
+  });
+
+  it(`should return 'false' for the valid password with 7 characters`, () => {
+    expect(checkPassword('Pa$sw0r')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the valid password with 17 characters`, () => {
+    expect(checkPassword('Pa$sw0rdPa$sw0rd1')).toBeFalsy();
+  });
+
+  it(`should return 'true' for the valid password with 16 characters`, () => {
+    expect(checkPassword('Pa$sw0rdPa$sw0rd')).toBeTruthy();
+  });
+
+  it(`should return 'false' for the password length 10 characters without 
+  special symbol`, () => {
+    expect(checkPassword('Passw0rd12')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the password length 10 characters without 
+  number`, () => {
+    expect(checkPassword('Pa$sword!!')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the password length 10 characters without 
+  uppercase letter`, () => {
+    expect(checkPassword('pa$sword12')).toBeFalsy();
+  });
 });
