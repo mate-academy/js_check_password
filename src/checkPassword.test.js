edcd2980c342a9ac2432759fcde5e1d72f4a43dc
coyotious <coyotious@gmail.com>
2021-07-08 16:07:36 +0300

    deleted one test

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -15,34 +15,36 @@ describe(`Function 'checkPassword':`, () => {
     expect(checkPassword('Pa$sw0rd')).toBeTruthy();
   });
 
-  it(`should return 'true' for the valid password with 12 characters`, () => {
-    expect(checkPassword('Pa$sw0rd1234')).toBeTruthy();
+  it(`should return 'false' if password includes cyryllic characters`, () => {
+    expect(checkPassword('Пароль!1')).toEqual(false);
   });
 
-  it(`should return 'false' for the valid password with 7 characters`, () => {
-    expect(checkPassword('Pa$sw0r')).toBeFalsy();
+  it(`should return 'false' if there is no uppercase
+  letters in password`, () => {
+    expect(checkPassword('password!1')).toEqual(false);
   });
 
-  it(`should return 'false' for the valid password with 17 characters`, () => {
-    expect(checkPassword('Pa$sw0rdPa$sw0rd1')).toBeFalsy();
+  it(`should return 'false' if there is no digits in password`, () => {
+    expect(checkPassword('Password!@')).toEqual(false);
   });
 
-  it(`should return 'true' for the valid password with 16 characters`, () => {
-    expect(checkPassword('Pa$sw0rdPa$sw0rd')).toBeTruthy();
+  it(`should return 'false' if there is no special symbols in password`, () => {
+    expect(checkPassword('Password22')).toEqual(false);
   });
 
-  it(`should return 'false' for the password length 10 characters without 
-  special symbol`, () => {
-    expect(checkPassword('Passw0rd12')).toBeFalsy();
+  it(`should return 'false' if there is 7 symbols in password`, () => {
+    expect(checkPassword('Passw@2')).toEqual(false);
   });
 
-  it(`should return 'false' for the password length 10 characters without 
-  number`, () => {
-    expect(checkPassword('Pa$sword!!')).toBeFalsy();
+  it(`should return 'true' if there is 12 symbols in password`, () => {
+    expect(checkPassword('Password22@!')).toEqual(true);
   });
 
-  it(`should return 'false' for the password length 10 characters without 
-  uppercase letter`, () => {
-    expect(checkPassword('pa$sword12')).toBeFalsy();
+  it(`should return 'true' if there is 16 symbols in password`, () => {
+    expect(checkPassword('ABCdejepgk%$@264')).toEqual(true);
+  });
+
+  it(`should return 'false' if there is 17 symbols in password`, () => {
+    expect(checkPassword('PFhrtdjvyw$&^pt34')).toEqual(false);
   });
 });
