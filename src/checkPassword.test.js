2c031fe25cd02982f92a432402d5f989ece32fab
mykolaPakhniuk <pakhniukm@gmail.com>
2021-10-20 14:20:37 +0300

    Solution

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,33 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(checkPassword).toBeTruthy();
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
-
+    expect(checkPassword('qweQwe1@')).toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return 'false' for the unvalid password with 7 characters`, () => {
+    expect(checkPassword('qwAwe1@')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the unvalid password 
+  with 17 characters`, () => {
+    expect(checkPassword('qwAasdasdasdasdasdwe1@')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the unvalid password 
+  without 1 special characters`, () => {
+    expect(checkPassword('qwAasdasdae1')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the unvalid password 
+  without 1 uppercase letter`, () => {
+    expect(checkPassword('qasdasdae1@')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the unvalid password without 1 digit`, () => {
+    expect(checkPassword('qasdasdaeA@')).toBeFalsy();
+  });
 });
