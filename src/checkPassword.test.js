087dac9ac172fa096133260e01951cf5ace03692
Corsika1634 <corsika1634@gmail.com>
2022-01-03 23:02:54 +0300

    Solution

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,66 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof checkPassword('')).toBe('boolean');
   });
 
-  it(`should return 'true' for the valid password with 8 characters`, () => {
-
+  it(`should return 'true' for the valid password //
+      (Password1!) with 8 characters`, () => {
+    expect(checkPassword('Password1!')).toBe(true);
   });
 
-  // write more tests here
+  it(`should return 'false' for the invalid password //
+      (Password11) with 8 characters`, () => {
+    expect(checkPassword('Password11')).toBe(false);
+  });
+
+  it(`should return 'true' for the valid password //
+      (Passwordpasswo1!) with 16 characters`, () => {
+    expect(checkPassword('Passwordpasswo1!')).toBe(true);
+  });
+
+  it(`should return 'false' for the invalid password //
+      (Passwordpassword) with 16 characters`, () => {
+    expect(checkPassword('Passwordpassword1!')).toBe(false);
+  });
+
+  it(`should return 'true' for the valid password //
+      contains only Latin letters (Qwertyuioplkj12!)`, () => {
+    expect(checkPassword('Qwertyuioplkj12!')).toBe(true);
+  });
+
+  it(`should return 'false' for the password //
+      contains any amount of non-Latin letters (Аwertyuioplkj12!)`, () => {
+    expect(checkPassword('Аwertyuioplkj12!')).toBe(false);
+  });
+
+  it(`should return 'true' for the valid password //
+      contains at least 1 uppercase letter (Qazqaz12!)`, () => {
+    expect(checkPassword('Qazqaz1!')).toBe(true);
+  });
+
+  it(`should return 'false' for the valid password //
+      doesn't contain the uppercase letter (qazqaz1!)`, () => {
+    expect(checkPassword('qazqaz1!')).toBe(false);
+  });
+
+  it(`should return 'true' for the valid password //
+      contains at least 1 digit (Qqqaaa1a!)`, () => {
+    expect(checkPassword('Qqqaaa1a!')).toBe(true);
+  });
+
+  it(`should return 'false' for the valid password //
+      contains no digits (Qqqaaaqa!)`, () => {
+    expect(checkPassword('Qqqaaaqa!')).toBe(false);
+  });
+
+  it(`should return 'true' for the valid password //
+      contains at least 1 special character (Q=qqaa*a1a!)`, () => {
+    expect(checkPassword('Q=qqaa*a1a!')).toBe(true);
+  });
+
+  it(`should return 'false' for the valid password //
+      contains at least 1 special character (Qfqqaafa1a)`, () => {
+    expect(checkPassword('Qfqqaafa1a')).toBe(false);
+  });
 });
