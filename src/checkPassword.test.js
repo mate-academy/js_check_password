b69cceb453fff43f4b65602da576f1a4e00174e3
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2022-11-05 19:32:25 +0200

    Tests

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -3,17 +3,54 @@
 describe(`Function 'checkPassword':`, () => {
   const checkPassword = require('./checkPassword');
 
-  it(`should be declared`, () => {
-    expect(checkPassword).toBeInstanceOf(Function);
+  it('should be declared', function() {
+    expect(checkPassword)
+      .toBeInstanceOf(Function);
   });
 
-  it(`should return boolean`, () => {
+  it('should return false'
+    + ' if password includes unacceptable letters', function() {
+    const result = checkPassword('пароль неприйнятний');
 
+    expect(result)
+      .toBeFalsy();
   });
 
-  it(`should return 'true' for the valid password with 8 characters`, () => {
+  it('should return false if password too short', function() {
+    const result = checkPassword('pasS1@');
 
+    expect(result)
+      .toBeFalsy();
   });
 
-  // write more tests here
+  it('should return false if password too long', function() {
+    const result = checkPassword('passwordistoolonG1@');
+
+    expect(result)
+      .toBeFalsy();
+  });
+
+  it('should return false'
+    + 'if password is not includes at least one number, special character'
+    + 'and uppercase letter', function() {
+    const testOne = checkPassword('Password!');
+    const testTwo = checkPassword('Password1');
+    const testThree = checkPassword('password1!');
+
+    expect(testOne)
+      .toBeFalsy();
+
+    expect(testTwo)
+      .toBeFalsy();
+
+    expect(testThree)
+      .toBeFalsy();
+  });
+
+  it('should return true if password is valid', function() {
+    const result = checkPassword('Password1!');
+
+    expect(result)
+      .toBeTruthy();
+  });
 });
