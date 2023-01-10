cdcd64763b8a663c383d30e17f2c75a5d04d82c7
Usenko Bohdana <usenkobogdana19@gmail.com>
2023-01-10 21:06:28 +0200

    solution

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,35 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
+    expect(typeof checkPassword('PaSsword0!')).toBe('boolean');
+  });
 
+  it(`should return 'false' for the password
+  with more than 16 characters`, () => {
+    expect(checkPassword('AAmdhet!nsmsjxkkkfc')).toBeFalsy();
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
-
+    expect(checkPassword('Passwo)1')).toBeTruthy();
   });
 
-  // write more tests here
+  it(`should return 'false' for the password
+    without 1 uppercase letter`, () => {
+    expect(checkPassword('password1')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the password
+  without 1 special character`, () => {
+    expect(checkPassword('Password1')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the password
+    without 1 digit`, () => {
+    expect(checkPassword('paSSword!')).toBeFalsy();
+  });
+
+  it(`should return 'false' for the password
+    contains cyrillic letters `, () => {
+    expect(checkPassword('Перемога1!')).toBeFalsy();
+  });
 });
