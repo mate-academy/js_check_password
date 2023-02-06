5bada4c9258ce5da20e4ec6673c5ba01a103d68c
Andrii Dydyk <andriidydyk622@gmail.com>
2023-02-06 18:04:32 +0100

    Solution

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,92 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof checkPassword('Password1@')).toBe('boolean');
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
+    const validPassword = checkPassword('Password1@');
 
+    expect(validPassword)
+      .toBe(true);
   });
 
-  // write more tests here
+  it('should accepts only letters of the Latin alphabet `Aa-Zz` ', () => {
+    const validPassword = checkPassword('Password1@');
+
+    expect(validPassword)
+      .toBe(true);
+  });
+
+  it(`should return 'false' 
+  if password consist letters of the Cyrylic alphabet 'Аа-Яя' `, () => {
+    const validPassword = checkPassword('Пароль@');
+
+    expect(validPassword)
+      .toBe(false);
+  });
+
+  it(`should return 'false' 
+  if the password at less 8 characters`, () => {
+    const validPassword = checkPassword('Passw1@');
+
+    expect(validPassword)
+      .toBe(false);
+  });
+
+  it(`should return 'true' if the password
+   contain maximum 16 characters inclusive`, () => {
+    const validPassword = checkPassword('Password1234567@');
+
+    expect(validPassword)
+      .toBe(true);
+  });
+
+  it(`should return 'false' if the password
+   contain more than 16 characters`, () => {
+    const validPassword = checkPassword('Password12345678@');
+
+    expect(validPassword)
+      .toBe(false);
+  });
+
+  it(`should return 'false' if the password
+   doesn't contain at least 1 digit`, () => {
+    const validPassword = checkPassword('Password@');
+
+    expect(validPassword)
+      .toBe(false);
+  });
+
+  it(`should return 'false' if the password
+   doesn't contain 1 special character`, () => {
+    const validPassword = checkPassword('Password1');
+
+    expect(validPassword)
+      .toBe(false);
+  });
+
+  it(`should return 'false' if the password
+   doesn't contain 1 uppercase letter`, () => {
+    const validPassword = checkPassword('password1@');
+
+    expect(validPassword)
+      .toBe(false);
+  });
+
+  it(`should return 'false' if the password
+    contain only uppercase letter`, () => {
+    const validPassword = checkPassword('PASSWORD1@');
+
+    expect(validPassword)
+      .toBe(false);
+  });
+
+  it(`should return 'false' if the password
+    contain space`, () => {
+    const validPassword = checkPassword('Pass word1@');
+
+    expect(validPassword)
+      .toBe(false);
+  });
 });
