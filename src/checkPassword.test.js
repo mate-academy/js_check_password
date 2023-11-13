1a2c9204ea0e9d2edbaf7f1b5f84a441537e81ad
Oleksii Bieliaiev <o.bieliaiev@icloud.com>
2023-11-13 15:07:01 +0200

    solution

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,58 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
+    const isValid = checkPassword('Password1!');
 
+    expect(typeof isValid).toBe('boolean');
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
+    const validPassword = 'Qwerty1!';
+    const isValid = checkPassword(validPassword);
 
+    expect(isValid).toBe(true);
   });
 
-  // write more tests here
+  it(`should return 'false' for a password without a special character`, () => {
+    const invalidPassword = 'Password123';
+    const isValid = checkPassword(invalidPassword);
+
+    expect(isValid).toBe(false);
+  });
+
+  it(`should return 'false' for a password without an uppercase letter`, () => {
+    const invalidPassword = 'password1!';
+    const isValid = checkPassword(invalidPassword);
+
+    expect(isValid).toBe(false);
+  });
+
+  it(`should return 'false' for a password without a digit`, () => {
+    const invalidPassword = 'Password!';
+    const isValid = checkPassword(invalidPassword);
+
+    expect(isValid).toBe(false);
+  });
+
+  it(`should return 'false' for a password with less than 8 characters`, () => {
+    const invalidPassword = 'Pass1!';
+    const isValid = checkPassword(invalidPassword);
+
+    expect(isValid).toBe(false);
+  });
+
+  it(`should return 'false' for a password with Cyrillic characters`, () => {
+    const invalidPassword = 'Пароль123!';
+    const isValid = checkPassword(invalidPassword);
+
+    expect(isValid).toBe(false);
+  });
+
+  it(`should return 'false' for a password with more
+  than 16 characters`, () => {
+    const invalidPassword = 'GiantMegaPassword123&#!';
+    const isValid = checkPassword(invalidPassword);
+
+    expect(isValid).toBe(false);
+  });
 });
