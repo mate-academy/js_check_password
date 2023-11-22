69f5cd3555844cbdceb85a9dfa6400ababe1fe80
VladSlob <141657806+VladSlob@users.noreply.github.com>
2023-11-22 08:01:05 +0200

    checkPassword.test.js

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,37 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    const result = checkPassword('Password1!');
+    expect(typeof result).toBe('boolean');
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
-
+    const result = checkPassword('Password1!');
+    expect(result).toBe(true);
   });
 
-  // write more tests here
+  it(`should return 'false' for the invalid password with less than 8 characters`, () => {
+    const result = checkPassword('Str@ng');
+    expect(result).toBe(false);
+  });
+
+  it(`should return 'false' for the invalid password without special characters`, () => {
+    const result = checkPassword('Password1');
+    expect(result).toBe(false);
+  });
+
+  it(`should return 'false' for the invalid password without uppercase letters`, () => {
+    const result = checkPassword('password1!');
+    expect(result).toBe(false);
+  });
+
+  it(`should return 'false' for the invalid password without digits`, () => {
+    const result = checkPassword('Password!');
+    expect(result).toBe(false);
+  });
+
+  it(`should return 'false' for the invalid password with more than 16 characters`, () => {
+    const result = checkPassword('Password1234567890!');
+    expect(result).toBe(false);
+  });
 });
