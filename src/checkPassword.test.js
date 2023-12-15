34151ab4d4af6a9ff21e5ffd39d2d22d9662f939
Aleksandr <cebanoleksandr@gmail.com>
2023-12-15 13:53:30 +0200

    solution

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,32 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
+    const result = checkPassword('password');
 
+    expect(typeof result).toBe('boolean');
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
-
+    expect(checkPassword('Pass@word1!')).toBe(true);
   });
 
-  // write more tests here
+  it(`should return 'false' for short password`, () => {
+    expect(checkPassword('Pass@1!')).toBe(false);
+  });
+
+  it(`should return 'false' for long password`, () => {
+    expect(checkPassword('Pass@dfgnhgdfhdghmfgfg1!')).toBe(false);
+  });
+
+  it(`should return 'false' for password without uppercase letter`, () => {
+    expect(checkPassword('pass@word1!')).toBe(false);
+  });
+
+  it(`should return 'false' for password without number`, () => {
+    expect(checkPassword('Pass@word!')).toBe(false);
+  });
+
+  it(`should return 'false' for password without special character`, () => {
+    expect(checkPassword('Password2')).toBe(false);
+  });
 });
