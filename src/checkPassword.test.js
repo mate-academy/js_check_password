92fc84dfea67d1c5f83c2abe74a770bdbfd454d8
Yehor Korablin <yehorkorablin@gmail.com>
2024-02-03 10:40:18 +0200

    Added tests

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,46 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof checkPassword('abcd')).toBe('boolean');
   });
 
-  it(`should return 'true' for the valid password with 8 characters`, () => {
-
+  it(`should return 'true'
+    for the valid password with 8 characters`, () => {
+    expect(checkPassword('Password1!')).toBe(true);
   });
 
-  // write more tests here
+  it(`should return 'false'
+    for the invalid password with 6 characters`, () => {
+    expect(checkPassword('qwerty')).toBe(false);
+  });
+
+  it(`should return 'false'
+    for the invalid password with 6 characters`, () => {
+    expect(checkPassword('Str@ng')).toBe(false);
+  });
+
+  it(`should return 'true'
+    for the valid password with 15 characters`, () => {
+    expect(checkPassword('Mate_Academy_#1')).toBe(true);
+  });
+
+  it(`should return 'false'
+    for the valid password with 21 characters`, () => {
+    expect(checkPassword('mynameisYehormyageis19!')).toBe(false);
+  });
+
+  it(`should return 'false'
+    for the invalid password with 8 characters`, () => {
+    expect(checkPassword('12345678')).toBe(false);
+  });
+
+  it(`should return 'false'
+    for the invalid password with 14 characters`, () => {
+    expect(checkPassword('qwerty12345678')).toBe(false);
+  });
+
+  it(`should return 'true'
+    for the valid password with 11 characters`, () => {
+    expect(checkPassword('qwerty1234!')).toBe(false);
+  });
 });
