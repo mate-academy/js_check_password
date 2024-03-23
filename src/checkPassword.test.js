fec020f6278dd2df3d0acd6a639f82d250da8b86
bla2001 <blasmartlab@gmail.com>
2024-03-23 22:19:34 +0200

    Solution

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,83 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
+    const result = checkPassword('Test@1234');
 
+    expect(typeof result).toBe('boolean');
   });
 
-  it(`should return 'true' for the valid password with 8 characters`, () => {
+  describe(`should return 'true' for valid passwords`, () => {
+    it(`with 8 symbols length and`
+    + ` 1 digit, 1 special character, 1 uppercase Latin letter`, () => {
+      const result = checkPassword('Abcdef1@');
+  
+      expect(result).toBe(true);
+    });
+  
+    it(`with 16 symbols length and`
+    + ` 1 digit, 1 special character, 1 uppercase Latin letter`, () => {
+      const result = checkPassword('Abcdef1@Abcdef1@');
+  
+      expect(result).toBe(true);
+    });
 
+    it(`with more then 8 and less then 16 symbols length`
+      + ` and 1 digit, 1 special character,`
+      + ` 1 uppercase Latin letter`, () => {
+      const result = checkPassword('Abcdef1@Abc');
+  
+      expect(result).toBe(true);
+    });
   });
 
-  // write more tests here
+  describe(`should return 'false' for invalid passwords`, () => {
+  it(`with empty string`, () => {
+      const result = checkPassword('');
+  
+      expect(result).toBe(false);
+    });
+
+    it(`with too small length`, () => {
+      const result = checkPassword('Abcde1@');
+  
+      expect(result).toBe(false);
+    });
+
+    it(`with too big length`, () => {
+      const result = checkPassword('Abcdef1@Abcdef1@Abcdef1@');
+  
+      expect(result).toBe(false);
+    });
+
+    it(`without special character`, () => {
+      const result = checkPassword('Abcdef1');
+  
+      expect(result).toBe(false);
+    });
+
+    it(`without digit`, () => {
+      const result = checkPassword('Abcdefg@');
+  
+      expect(result).toBe(false);
+    });
+
+    it(`with digits only`, () => {
+      const result = checkPassword('12345678');
+  
+      expect(result).toBe(false);
+    });
+
+    it(`with letters only`, () => {
+      const result = checkPassword('Abcdefgh');
+  
+      expect(result).toBe(false);
+    });
+
+    it(`with non-Latin letters`, () => {
+      const result = checkPassword('AbcdЖд1@');
+  
+      expect(result).toBe(false);
+    });
+
+  });
 });
